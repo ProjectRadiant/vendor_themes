8ea13a2f2fc88e29877dd2507789c17be1d37b5d
Maitreya29 <maitreyapatni30@gmail.com>
2021-01-19 17:53:32 +0530

    Themes: Let's only stick to themes and switches for now

--- themes.mk
+++ themes.mk
@@ -14,14 +14,6 @@ PRODUCT_PACKAGES += \
     MaterialOceanSystemUIOverlay \
     DarkGreySystemOverlay \
     DarkGreySystemUIOverlay 
-
-# QS header styles
-PRODUCT_PACKAGES += \
-    QSHeaderBlack \
-    QSHeaderGrey \
-    QSHeaderLightGrey \
-    QSHeaderAccent \
-    QSHeaderTransparent
 	
 # Switch themes
 PRODUCT_PACKAGES += \
@@ -32,110 +24,7 @@ PRODUCT_PACKAGES += \
     Retro \
     Stockish \
     Narrow
-	
-# Accents
-PRODUCT_PACKAGES += \
-    BrownAccent \
-    TealAccent \
-    BlackAccent \
-    YellowAccent \
-    RedAccent \
-    PurpleAccent \
-    PinkAccent \
-    OrangeAccent \
-    GreenAccent \
-    WhiteAccent \
-    CyanAccent \
-    ObfusBleu \
-    NotImpPurple \
-    Holillusion \
-    MoveMint \
-    AlmostProBlue \
-    BubblegumPink \
-    FrenchBleu \
-    Stock \
-    ManiaAmber \
-    DrownedAquaBlue \
-    BoucheRed \
-    DreamyPurple \
-    GrapesPurple \
-    SpookedPurple \
-    MisleadingRed \
-    FSRGrey \
-    HeirloomBleu \
-    LunaBlue \
-    WarmthOrange \
-    NaturedGreen \
-    ColdBleu \
-    DiffDayGreen \
-    DuskPurple \
-    BurningRed \
-    SimilarDawnRed \
-    SpoofyGreen \
-    NewHouseOrange \
-    LimedGreen \
-    SunsetOrange \
-    UiGradientsDIMIGO
 
-# QS tile styles
-PRODUCT_PACKAGES += \
-    QSTileDefault \
-    QSTileCircleTrim \
-    QSTileCircleDualTone \
-    QSTileCircleGradient \
-    QSTileCookie \
-    QSTileDottedCircle \
-    QSTileDualToneCircle \
-    QSTileInk \
-    QSTileInkdrop \
-    QSTileMountain \
-    QSTileNinja \
-    QSTileOreo \
-    QSTileOreoCircleTrim \
-    QSTileOreoSquircleTrim \
-    QSTilePokesign \
-    QSTileSquaremedo \
-    QSTileSquircle \
-    QStileBadgetwo \
-    QSTileSquircleTrim \
-    QSTileTeardrop \
-    QStileDiamond \
-    QStileHexagon \
-    QStileSquare \
-    QSTileWavey \
-    QStileGear \
-    QStileStar \
-    QStileBadge \
-    QStileNeonlike \
-    QStileOOS \
-    QStileTriangles \
-    QStileDivided \
-    QStileCosmos \
-    QStileRoundedRect
-    
-# Notification themes
-PRODUCT_PACKAGES += \
-    NotificationBlackTheme \
-    NotificationDarkTheme \
-    NotificationLightTheme
-	
-# Brightness sliders Styles
-PRODUCT_PACKAGES += \
-    BrightnessSliderDefault \
-    BrightnessSliderDaniel \
-    BrightnessSliderMemeMini \
-    BrightnessSliderMemeRound \
-    BrightnessSliderMemeRoundStroke \
-    BrightnessSliderMemeStroke
-	
-# NavBar themes
-PRODUCT_PACKAGES += \
- 	StockAsusOverlay \
-    NavbarAsusOverlay \
-    NavbarOnePlusOverlay \
-    NavbarOneUiOverlay \
-    NavbarTecnoCamonOverlay
-	
 # UI Styles
 PRODUCT_PACKAGES += \
     UiStyleDefault \

44099fc10a52f7e5b7e0205e83fe22d412797c04
Maitreya29 <maitreyapatni30@gmail.com>
2021-01-19 22:22:58 +0530

    Revert "overlay: packages: Set the styles and wallpapers package"
    This reverts commit 7b526a951bff2ae44d5dc7410cc58ffa00cc978b.

--- themes.mk
+++ themes.mk
@@ -1,9 +1,3 @@
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
-
-# Allow overlays to be excluded from enforcing RRO
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
-
 # Themes
 PRODUCT_PACKAGES += \
     ClearSpringSystemOverlay \
